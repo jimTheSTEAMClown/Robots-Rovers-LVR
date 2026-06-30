6064862eb11380cfea07e2ed21271d328b2dbfa8
Jim Burnham <jim.the.steam.clown@gmail.com>
2026-06-29 20:02:31 -0700

    Update jazzy_ros2_source.sh

--- ROS2/jazzy_ros2_source.sh
+++ ROS2/jazzy_ros2_source.sh
@@ -3,17 +3,7 @@
 # https://docs.ros.org/en/jazzy/Installation/Alternatives/Ubuntu-Development-Setup.html
 
 echo 'Ready to source your jazzy ros2 environment?'
-read -s -p "printenv | grep -i ROS - Press Enter to continue..."
-printenv | grep -i ROS
-
-read -s -p "next will make sure you are in ~/ros2_jazzy/ - Press Enter to continue..."
-cd ~/ros2_jazzy/
-
-# Setup environment - Set up your environment by sourcing the following file.
-read -s -p ". ~/ros2_jazzy/install/local_setup.bash - Press Enter to continue..."
-. ~/ros2_jazzy/install/local_setup.bash
-
-read -s -p "I think we are all setup.  Ready to test? - Press Enter to continue..."
+read -s -p "Press Enter to continue..."
 
 echo 'Try some examples'
 echo 'In one terminal, source the setup file and then run a C++ talker: '
@@ -23,3 +13,34 @@ echo ' '
 echo 'In another terminal source the setup file and then run a Python listener: '
 echo '. ~/ros2_jazzy/install/local_setup.bash'
 echo 'ros2 run demo_nodes_py listener'
+
+echo "----------------------------------------------------"
+echo "Do you wish to run ROS Talker or Listener?  Enter t/T or l/L or any Key to quit?"
+read -p "Talk or Listen?: " ros2Mode
+# elif statements
+if [ $ros2Mode == "t" ] || [ $ros2Mode == "T" ]
+then
+echo "----------------------------------------------------"
+echo "changing to the ros2_jazzy"
+cd ~/ros2_jazzy/
+echo "sourcing ~/ros2_jazzy/install/local_setup.bash"
+. ~/ros2_jazzy/install/local_setup.bash
+echo "ros2 run demo_nodes_cpp talker"
+ros2 run demo_nodes_cpp talker
+echo "----------------------------------------------------"
+echo "Done running ROS as a Talker"
+echo "----------------------------------------------------"
+elif [ $ros2Mode == "l" ] || [ $ros2Mode == "L" ]
+echo "----------------------------------------------------"
+echo "changing to the ros2_jazzy"
+cd ~/ros2_jazzy/
+echo "sourcing ~/ros2_jazzy/install/local_setup.bash"
+. ~/ros2_jazzy/install/local_setup.bash
+echo "ros2 run demo_nodes_py listener"
+ros2 run demo_nodes_py listener
+echo "----------------------------------------------------"
+echo "Done running ROS as a Listener"
+echo "----------------------------------------------------"
+else
+echo "Skipping ROS source"
+fi
